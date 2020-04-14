85053ace2f08abfd64c3dcf0145b4eeb524f36c6
Mark Lewis <mlewis@trinity.edu>
2020-04-14 07:19:02 -0500

    Setup for class on Slinky.

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,6 +4,12 @@ import org.scalajs.dom
 import org.scalajs.dom.document
 import scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
+import org.scalajs.dom.html
+
+import slinky.core._
+import slinky.web.ReactDOM
+import slinky.web.html._
+
 
 object Hello {
   def main(args: Array[String]): Unit = {
@@ -12,9 +18,17 @@ object Hello {
     println("Lewis isn't lying...much.")
 
     document.getElementById("title").innerHTML = "Scala.js"
-    document.getElementById("content").innerHTML = "No more dealing with straight JavaScript!"
+    document.getElementById("content").innerHTML =
+      "No more dealing with straight JavaScript!"
     document.getElementById("inclass").innerHTML = "More text"
     appendPar(document.body, "Hello World")
+
+    drawSomethingOnCanvas(document.getElementById("canvas").asInstanceOf[html.Canvas])
+
+    ReactDOM.render(
+      h1("Hello, world!"),
+      document.getElementById("react-root")
+    )
   }
 
   @JSExportTopLevel("addClickedMessage")
@@ -29,4 +43,8 @@ object Hello {
     targetNode.appendChild(parNode)
   }
 
+  def drawSomethingOnCanvas(canvas: html.Canvas): Unit = {
+    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+    ctx.fillRect(100, 100, 100, 100)
+  }
 }
